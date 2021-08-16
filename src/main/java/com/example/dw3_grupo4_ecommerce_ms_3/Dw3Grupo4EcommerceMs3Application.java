2cf80bf9470e9f6096ac158beeda50519058fb9d
LuisFelipeCruz <luisfelipecruzgeo@hotmail.com>
2021-08-15 22:08:51 -0500

    feat(springbBootInit): archivos iniciales del proyecto

---
+++ src/main/java/com/example/dw3_grupo4_ecommerce_ms_3/Dw3Grupo4EcommerceMs3Application.java
@@ -0,0 +1,13 @@
+package com.example.dw3_grupo4_ecommerce_ms_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dw3Grupo4EcommerceMs3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dw3Grupo4EcommerceMs3Application.class, args);
+    }
+
+}
